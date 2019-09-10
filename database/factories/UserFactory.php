73774597d765f52ab8da5a6d500376e40e74a7bd
Axieum <7577851+Axieum@users.noreply.github.com>
2019-09-10 18:45:55 +1000

    Formatting; PHPDocs

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,12 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
+
 use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +19,19 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+/**
+ * The hashed password used for all generated users.
+ *
+ * @var string
+ */
+$password = Hash::make('password');
+
+$factory->define(User::class, function (Faker $faker) use ($password) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => $password,
         'remember_token' => Str::random(10),
     ];
 });
